506aa657283803bc32700d950ec147dc170c06ff
charlyol <cha.olingergmail.com>
2024-08-06 20:03:01 +0000

    make vmc ok

--- src/View/home.php
+++ src/View/home.php
@@ -4,39 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <title>Home - Fake Store</title>
+    <title>APG Home</title>
+    <link rel="icon" type="image/x-icon" href="../../public/assets/img/gallery/favicon.ico">
 </head>
 <body>
 <div class="container">
     <h1 class="mt-4">Welcome to the Fake Store</h1>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://via.placeholder.com/800x400" class="d-block w-100" alt="Slider Image 1">
-            </div>
-            <div class="
-
-carousel-item">
-                <img src="https://via.placeholder.com/800x400" class="d-block w-100" alt="Slider Image 2">
-            </div>
-            <div class="carousel-item">
-                <img src="https://via.placeholder.com/800x400" class="d-block w-100" alt="Slider Image 3">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </a>
+    <div>
+        carouselle*
     </div>
 
     <h2 class="mt-4">Categories</h2>
